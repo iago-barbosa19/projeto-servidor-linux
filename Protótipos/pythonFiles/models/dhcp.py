35c91421f144d6dc209dcf49a255baddf3c4f744
Iago Barbosa <iagofbarbosa23@gmail.com>
2021-11-07 21:14:29 -0300

    Teste DNS V2.01 && Teste DHCP V2.03
    Versões ainda em fase de teste, e está sujeitas a possíveis alterações

--- Protótipos/pythonFiles/models/dhcp.py
+++ Protótipos/pythonFiles/models/dhcp.py
@@ -8,7 +8,7 @@ class Dhcp(Ip):
         super().__init__(ipv4, gateway, dns1, dns2, subNetMask)
         self.__dhcpPoolInicial = dhcpPoolInicial
         self.__dhcpPoolFinal = dhcpPoolFinal
-        self._Ip__networkIpSetter(self._Ip__ipv4, self._Ip__subNetMask)
+        self.__networkIp = self.networkIpSetter(self.ipv4, self.subNetMask)
 
     @property
     def dhcpPoolInicial(self) -> None:
@@ -19,24 +19,29 @@ class Dhcp(Ip):
         return self.__dhcpPoolFinal
 
     def dhcpConf(self:object) -> None:
+        """Método que configura o serviço de DHCP.
+        O método configura uma pool DHCP de acordo com os dados que forem inseridos e passados para a classe.
+        O arquivo é modificado direto no diretório /etc/dhcp e /etc/default.
+        
+        """
         os.chdir('/etc/dhcp')
-        with open('dhcpd.conf', 'r') as dhcpConfig:
+        with open('dhcpd.conf', 'r+') as dhcpConfig:
             lines = 0
             temporaryData = []
             dhcpConfig.seek(0)
             for data in dhcpConfig.readlines():
                 temporaryData.append(data)
             for data in temporaryData:
-                if f'#DHCP REDE:{self.networkIpSetter(self._Ip__ipv4, self._Ip__subNetMask)}\n' == data:
+                if f'#DHCP Rede:{self.__networkIp}\n' == data:
                     os.system('echo Essa rede já esta cadastrada.')
                     break
                 elif lines == (len(temporaryData) - 1):
                     with open('dhcpd.conf', 'a') as dhcpd:
-                        dhcpd.write(f'\n\n#DHCP Rede:{self.networkIpSetter(self._Ip__ipv4, self._Ip__subNetMask)}\nsubnet {self.networkIpSetter(self.ipv4, self.subNetMask)} netmask {self.subNetMask}'\
-                                        ' {\n  range'\
-                                        f' {self.dhcpPoolInicial} {self.dhcpPoolFinal};\n  option routers {self._Ip__gateway};\n  '\
-                                        f'option domain-name-servers {self._Ip__dns1}, {self._Ip__dns2};\n'\
-                                        '}')
+                        dhcpd.write(f'\n\n#DHCP Rede:{self.networkIp}\nsubnet {self.networkIpSetter(self.ipv4, self.subNetMask)} netmask {self.subNetMask}'\
+                                    ' {\n  range'\
+                                    f' {self.dhcpPoolInicial} {self.dhcpPoolFinal};\n  option routers {self.gateway};\n  '\
+                                    f'option domain-name-servers {self.dns1}, {self.dns2};\n'\
+                                    '}')
                     dhcpConfig.seek(0)
                 
         os.system(f'cp -p /home/{os.getlogin()}/Config_Saves_PSC/dhcpd.conf /etc/dhcp/')
@@ -55,16 +60,20 @@ class Dhcp(Ip):
     
     
     def saveSettings(self:object) -> None:
+        """Função para salvar as configurações em um txt."""
         try:
             os.chdir(f'/home/{os.getlogin()}')
             os.mkdir('Config_Saves_PSC')
         except FileExistsError:
             os.chdir(f'/home/{os.getlogin()}/Config_Saves_PSC')
         with open('ConfigDHCP.txt', 'a+') as dhcpSave:
-            dhcpSave.write(f'IPV4:{self._Ip__ipv4}|Gateway{self._Ip__gateway}|DNS1{self._Ip__dns1}|DNS2{self._Ip__dns2}|Máscara de Sub-Rede{self._Ip__subNetMask}'\
-                           f'NetworkIp: {self._Ip__networkIpSetter(self._Ip__ipv4, self._Ip__subNetMask)}|Pool Inicial do DHCP:{self.dhcpPoolInicial}|Pool Final do DHCP:{self.dhcpFinal}'\
+            dhcpSave.write(f'IPV4:{self.ipv4}|Gateway{self.gateway}|DNS1{self.dns1}|DNS2{self.dns2}|Máscara de Sub-Rede{self.subNetMask}'\
+                           f'NetworkIp: {self.networkIp}|Pool Inicial do DHCP:{self.dhcpPoolInicial}|Pool Final do DHCP:{self.dhcpFinal}'\
                            f'\nData da modificação:{datetime.datetime.now()}')
 
+    def __repr__(self):
+        print('Os métodos que é possível visualizar as Docstrings:\n\ndhcpConf\nsaveSettings\n\n'\
+              'Em casos de dúvidas no uso do programa, consulte-as.')
 
 if __name__ == '__main__':
     raise NotImplementedError('\nErro de Inicialização. \nInicialize o arquivo principal para o funcionamento correto.')
--- Protótipos/pythonFiles/models/dns.py
+++ Protótipos/pythonFiles/models/dns.py
@@ -1,26 +1,37 @@
+from models.ip import Ip
 import os, datetime
 
 
-class Dns():
+class Dns(Ip):
     
     def __init__(self:object, ipv4:str, subNetMask:str, domain:str, nomeServer: str) -> None:
-        self.__ipv4: str = ipv4
-        self.__subNetMask: str = subNetMask
+        super().__init__(ipv4, subNetMask,)
         self.__domain:str = domain
         self.__nomeServer:str = nomeServer
 
     def changeDnsBind9(self:object) -> None:
-        """
-        Configuração do Serviço DNS por parte do Bind9
+        """Configuração do serviço Bind9. Por meio dele que é possível ser feito o NAT.
+        Esse serviço seria para a configuração da página local de rede.
+
+        1 - Primeira parte seria a configuração do DNS Zona Direta.
+        2 - Segunda parte seria a configuração do DNS Zona Indireta.
+
+        Os arquivos db.local e db.0 respectivamente são copiados para a mesma página, com nomes devidamente alterados. 
+
+        nomeArquivo| A variável domínio é repartida de acordo com a pontuação, separando assim o domínio.
+        db.{nomeArquivo[0]}| Ele seria a cópia do nomeArquivo db.local. Usária o indíce 0 da lista. O conteúdo desse indíce seria o nome
+        do domínio.
+
+
         """
         os.chdir('/etc/bind')
-        arquivo = self.__domain.split('.')
+        nomeArquivo = self.__domain.split('.')
         try:
-            os.system(f'cp -p db.local db.{arquivo[0]}')
+            os.system(f'cp -p db.local db.{nomeArquivo[0]}')
         except FileExistsError:
-            os.system(f'rm db.{arquivo[0]}')
-            os.system(f'cp -p db.local db.{arquivo[0]}')
-        with open(f'db.{arquivo[0]}', 'w+') as dbAdminFile:
+            os.system(f'rm db.{nomeArquivo[0]}')
+            os.system(f'cp -p db.local db.{nomeArquivo[0]}')
+        with open(f'db.{nomeArquivo[0]}', 'w+') as dbAdminFile:
             dbAdminFile.write(f'\n;\n; BIND data file for local loopback interface\n;\n$TTL    604800\n'\
                             f'@       IN      SOA     {self.__domain}. root.{self.__domain}. (\n'\
                             f'                             2         ; Serial\n'\
@@ -29,8 +40,9 @@ class Dns():
                             f'                       2419200         ; Expire\n'\
                             f'                        604800 )       ; Negative Cache TTL\n;\n'\
                             f'@       IN      NS      {self.__domain}.\n@       IN      A       127.0.0.1\n'\
-                            f'www     IN      A       {self.__ipv4}\n'\
-                            f'ftp     IN      A       {self.__ipv4}')
+                            f'www     IN      A       {self.ipv4}\n'\
+                            f'ftp     IN      A       {self.ipv4}')
+        
         with open('named.conf.default-zones', 'r') as defaultZones:
             lines = 0
             temporaryData = []
@@ -44,33 +56,62 @@ class Dns():
                         with open('named.conf.default-zones', 'a') as defaultZones1:
                             defaultZones1.write(f'// zona {self.__domain}\nzone "{self.__domain}" '\
                                             '{\n        type master;\n        '\
-                                            f'file "/etc/bind/db.{arquivo[0]}";\n'\
+                                            f'file "/etc/bind/db.{nomeArquivo[0]}";\n'\
                                             '};\n')
                     lines += 1
     
     def changeDnsApache2(self:object) -> None:
-        """
-        Configuração do Serviço DNS por parte do Apache2.
+        """Configuração do serviço Apache2.
+        Por meio desta configuração que é possível que o BIND9 funcione de maneira adequada e acesse sites.
+
+        O método executa cópia o nomeArquivo 000-default.conf para a mesma página, no entanto com o nome do usuário logado.
+        Como o nomeArquivo 000-default.conf já tem as permissões pré-definidas durante a instalação do serviço apache2, foi decidido
+        copiar o nomeArquivo e alterá-lo, pois desta forma era possível ter certeza de que erros devido à falta de permissões não ocorreriam.
+
+        Após executar a cópia, as alterações começam a serem feitas nela.
+        
+        Caso o nomeArquivo já exista, é possível sobrescreve-lo, ou deixá-lo intacto.
+
+        Esse método também cria pasta sites, localizada no diretório /etc/www
+
+        Nessa pasta vai ficar guardado a página html index.
+        Essa vai ser a página principal do DNS.
         """
         os.chdir('/etc/apache2/sites-available')
         try:
             os.mkdir('/var/www/sites')
         except FileExistsError:
             pass
-        with open(f'{os.getlogin()}.conf', 'w+') as apacheArquivo:
-            apacheArquivo.write(f"<VirtualHost *:80>\n        ServerName www.{self.__nomeServer}\n\n        ServerAlias www.{self.__domain}\n        "\
-                                f"ServerAdmin webmaster@localhost\n        DocumentRoot /var/www/sites\n        ErrorLog"\
-                                " ${APACHE_LOG_DIR}/error.log\n        CustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>")
-            os.system(f'a2ensite {os.getlogin()}.conf')
+        with open('/var/www/sites/index.html', 'w') as index:
+            index.write('<html>\n<head>\n<title>Index</title>\n</head>\n<body>\n<h1 style="color: #333; font-size:1.5rem;'\
+                        'margin: 1000px 1000px; ">Página Principal funcionando</h1>\n</body>\n</html>')
+        try:
+            os.system(f'cp -p ./000-default.conf ./{os.getlogin()}.conf')
+            with open(f'{os.getlogin()}.conf', 'w+') as apacheArquivo:
+                apacheArquivo.write(f"<VirtualHost *:80>\n        ServerName www.{self.__nomeServer}\n\n        ServerAlias www.{self.__domain}\n        "\
+                                    f"ServerAdmin webmaster@localhost\n        DocumentRoot /var/www/sites\n        ErrorLog"\
+                                    " ${APACHE_LOG_DIR}/error.log\n        CustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>")
+                os.system(f'a2ensite {os.getlogin()}.conf')
+        except FileExistsError:
+            os.system(f'echo O nomeArquivo já existe. Gostaria mesmo assim de sobrescreve-lo?\ny/n')
+            if opc:= input() == 'y':
+                with open(f'{os.getlogin()}.conf', 'w+') as apacheArquivo:
+                    apacheArquivo.write(f"<VirtualHost *:80>\n        ServerName www.{self.__nomeServer}\n\n        ServerAlias www.{self.__domain}\n        "\
+                                        f"ServerAdmin webmaster@localhost\n        DocumentRoot /var/www/sites\n        ErrorLog"\
+                                        " ${APACHE_LOG_DIR}/error.log\n        CustomLog ${APACHE_LOG_DIR}/access.log combined\n</VirtualHost>")
+                os.system(f'a2ensite {os.getlogin()}.conf')
+            else:
+                pass
             
     def dnsConf(self:object) -> None:
-        """Função que vai efetuar a configuração do DNS"""
+        """Função que inicializa as configurações do DNS. 
+        Ela inicializa os métodos de configuração de cada um dos serviços.
+        Esse método que é chamado para configurar o DNS no geral."""
         self.changeDnsBind9()
         self.changeDnsApache2()
-        os.system("/etc/init.d/apache2 restart")
+        os.system("systemctl restart apache2")
         os.system("systemctl restart bind9")
-        os.system("systemctl status bind9")
-        os.system('echo Serviço concluído.')
+        os.system('echo Configuração terminada.\nVoltando para página principal.')
         
     def saveSettings(self:object) -> None:
         """Função para salvar as configurações em um txt."""
@@ -85,6 +126,9 @@ class Dns():
                         f'{datetime.datetime.now()}')
         os.system('echo A configuração foi salva com sucesso')
 
+    def __repr__(self):
+        print('Os métodos que é possível visualizar as Docstrings:\ndnsConf\nchangeDnsApache2\nchangeDnsBind9\nsaveSettings\n\n'\
+              'Em casos de dúvidas no uso do programa, consulte-as.')
 
 if __name__ == '__main__':
-    raise NotImplementedError('Esse arquivo não pode ser inicializado como principal')
+    raise NotImplementedError('Esse nomeArquivo não pode ser inicializado como principal')
