6e1731fe0c37bdc9c697a02e382b293b780425a0
Iago Barbosa <iagofbarbosa23@gmail.com>
2021-11-17 21:24:26 -0300

    Protótipo Flask V1.0

--- Protótipos/pythonFiles/prototipoFlask.py
+++ Protótipos/pythonFiles/prototipoFlask.py
@@ -22,26 +22,30 @@ def index():
 def home():
     if request.method == 'GET':
         return render_template('home.html')
-    else:
-        if request.form['tipoConfig'] == 'configIp':
+    
+@software.route('/alterar', methods=[ 'GET','POST'])
+def alterar():
+    if request.form['tipoConfig'] == 'configIp':
             if request.form['dnsChoice'] == 'dnsProprio':
-                configIp = Ip(ipv4=request.form['ipv4'], gateway=request.form['gateway'], subNetMask=request.form['subnetMask'],
+                configIp = Ip(ipv4=request.form['ipv4'], gateway=request.form['gateway'], subNetMask=request.form['subNetMask'],
                             dns1=request.form['dns1'], dns2=request.form['dns2'])
             else:
-                configIp = Ip(ipv4=request.form['ipv4'], gateway=request.form['gateway'], subNetMask=request.form['subnetMask'],
+                configIp = Ip(ipv4=request.form['ipv4'], gateway=request.form['gateway'], subNetMask=request.form['subNetMask'],
                             dns1='8.8.8.8', dns2='8.8.4.4')
             configIp.ipConf()
-        elif request.form['tipoConfig'] == 'configDhcp':
-            if request.form['dnsChoice'] == 'dnsProprio':
-                configDhcp = Dhcp(ipv4=request.form['ipv4'], gateway=request.form['gateway'], subNetMask=request.form['subnetMask'],
-                            dns1=request.form['dns1'], dns2=request.form['dns2'],
-                            dhcpPoolInicial=request.form['dhcpPoolInicial'], dhcpPoolFinal=request.form['dhcpPoolFinal'])
-            else:
-                configDhcp = Dhcp(ipv4=request.form['ipv4'], gateway=request.form['gateway'], subNetMask=request.form['subnetMask'],
-                            dns1='8.8.8.8', dns2='8.8.4.4',
-                            dhcpPoolInicial=request.form['dhcpPoolInicial'], dhcpPoolFinal=request.form['dhcpPoolFinal'])
-            configDhcp.dhcpConf()
-        elif request.form['tipoConfig'] == 'configDns':
-            configDns = Dns(ipv4=request.form['ipv4'], domain=request.form['domain'], serverName=request.form['serverName'])
-            configDns.dnsConf()
-    
+    elif request.form['tipoConfig'] == 'configDhcp': 
+        if request.form['dnsChoice'] == 'dnsProprio': 
+            # Itens necessários no DHCP:
+            # IPV4, gateway, DNS1 e DNS2, subnetMask, DhcpPoolInicial e DhcpPoolFinal
+           configDhcp = Dhcp(ipv4=request.form['ipv4'], gateway=request.form['gateway'], subNetMask=request.form['subNetMask'],
+                             dns1=request.form['dns1'], dns2=request.form['dns2'],
+                             dhcpPoolInicial=request.form['rangeInicial'], dhcpPoolFinal=request.form['rangeFinal'])
+        else:
+            configDhcp = Dhcp(ipv4=request.form['ipv4'], gateway=request.form['gateway'], subNetMask=request.form['subNetMask'],
+                        dns1='8.8.8.8', dns2='8.8.4.4',
+                        dhcpPoolInicial=request.form['rangeInicial'], dhcpPoolFinal=request.form['rangeFinal'])
+        configDhcp.dhcpConf()
+    else: # Configuração do DNS
+        configDns = Dns(ipv4=request.form['ipv4'], subNetMask=request.form['subNetMask'], domain=request.form['domain'], serverName=request.form['serverName'])
+        configDns.dnsConf()
+    return redirect(url_for('home'))
